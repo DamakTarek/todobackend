5b20b023f93d99d2f6a713bfb63fd526f417e2f3
Tarek <steamtarek@gmail.com>
2018-11-22 14:30:28 +0100

    az

--- src/todobackend/urls.py
+++ src/todobackend/urls.py
@@ -8,15 +8,17 @@ Function views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Ho me.as_view(), name='home')
 Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
-
+ 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('todo.urls'))
 ]
